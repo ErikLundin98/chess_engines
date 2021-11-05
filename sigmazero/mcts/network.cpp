8fcbb7e36fa15bd293c9e9f26ac6855d8d412d13
Justus Karlsson <juska933@student.liu.se>
2021-11-05 17:01:14 +0100

    Working action encoding.

--- sigmazero/mcts/network.cpp
+++ sigmazero/mcts/network.cpp
@@ -4,7 +4,9 @@
 #include <algorithm>
 #include <cmath>
 #include <utility>
+#include <cassert>
 #include <set>
+
 #define HANDCRAFTED_NETWORK
 #ifdef HANDCRAFTED_NETWORK
 #include <eval/eval.hpp>
@@ -28,7 +30,7 @@ Network::Evaluation Network::evaluate(chess::position& state) const{
             chess::undo undo = state.make_move(move);
             double value = -eval::evaluate(state);
             state.undo_move(move, undo);
-            size_t action = action_from_move(state, move);
+            size_t action = action_from_move(move);
             action_logits[action] = value;
         }
     }
@@ -37,7 +39,7 @@ Network::Evaluation Network::evaluate(chess::position& state) const{
     std::vector<chess::move> legal_moves{state.moves()};
     double exp_sum = 0.0;
     for (chess::move move: legal_moves) {
-        size_t a = action_from_move(state, move);
+        size_t a = action_from_move(move);
         result.action_probabilities[a] = std::exp(action_logits[a]);
         exp_sum += result.action_probabilities[a];
     }
@@ -49,9 +51,10 @@ Network::Evaluation Network::evaluate(chess::position& state) const{
     
 }
 
+bool Network::encoding_initialized = false;
 
 // X-Y coords -> direction index
-std::pair<int, int> knight_directions[] = {
+std::pair<int, int> Network::knight_directions[8] = {
     std::make_pair(1,2),
     std::make_pair(2,1),
     std::make_pair(2,-1),
@@ -62,7 +65,7 @@ std::pair<int, int> knight_directions[] = {
     std::make_pair(-1,2)
 };
 // X-Y coords
-std::pair<int, int> queen_directions[] = {
+std::pair<int, int> Network::queen_directions[8] = {
     std::make_pair(0,1),
     std::make_pair(1,1),
     std::make_pair(1,0),
@@ -73,98 +76,105 @@ std::pair<int, int> queen_directions[] = {
     std::make_pair(-1,1)
 };
 
+chess::piece Network::underpromotions[3] = {
+    chess::piece_knight,
+    chess::piece_bishop,
+    chess::piece_rook
+};
+const std::string Network::QUEEN_ACTION = "Queen";
+const std::string Network::KNIGHT_ACTION = "Knight";
+const std::string Network::UNDERPROMOTION_ACTION = "Underpromotion";
 
-// Regular < 0 | 3 dir bits | 3 magnitude bits / knight | 6 from square bits > 
-// Promotion: < 1 | 0000 | 2 dir bits (NW, N, NE) | 2 promotion piece bits | 1 top/lower side of board bot | 3 x-value bits>
-// Max value: 0b1000011111111 = 4351
-// vs 64*73 = 4672 (?????)
-size_t Network::action_from_move(const chess::position& state, const chess::move& move) {
-    auto [side, piece] = state.pieces().get(move.from);
-    int delta_x = (move.to % 8) - (move.from % 8);
-    int delta_y = (move.to / 8) - (move.from / 8);
-    
-    // Under Promotion
-    if (move.promote != chess::piece::piece_none && move.promote != chess::piece::piece_queen) {
-        size_t piece = 0;
-        switch(move.promote){
-            case chess::piece::piece_knight: piece = 0; break;
-            case chess::piece::piece_bishop: piece = 1; break;
-            case chess::piece::piece_rook: piece = 2; break;
-            default: std::cerr << "Underpromotion is not valid" << std::endl;
+Network::Action Network::actions[64*73];
+std::map<std::tuple<size_t, int, int, size_t>, size_t> Network::queen_actions;
+std::map<std::tuple<size_t, int, int>, size_t> Network::knight_actions;
+std::map<std::tuple<size_t, int, size_t>, size_t> Network::underpromotion_actions;
+
+void Network::initialize_encoding_map(){
+    size_t action = 0;
+    for (size_t pos = 0; pos < 64; pos++) {
+        for (int dir = 0; dir < 8; dir++) {
+            for (int magnitude = 1; magnitude <= 7; magnitude++) {
+                auto [dx, dy] = queen_directions[dir];
+                queen_actions[std::make_tuple(pos, dx, dy, magnitude)] = action;
+                actions[action++] = {QUEEN_ACTION, pos, dx, dy, magnitude};
+            }
+            auto [dx, dy] = knight_directions[dir];
+            knight_actions[std::make_tuple(pos, dx, dy)] = action;
+            actions[action++] = {KNIGHT_ACTION, pos, dx, dy};
         }
-        size_t dir = delta_x + 1; // (-1, 0, 1) -> (0, 1, 2)
-        size_t underprom_idx = dir*3 + piece;
-        return 1 << (6+3+3) | underprom_idx << (6);
-    }
-    size_t action = (size_t) move.from;
-    auto get_dir_idx = [](std::pair<int, int> arr[], std::pair<int, int> item){
-        auto it = std::find(arr, arr+8, item);
-        if (it == arr+8) {
-            std::cout << "Too big index" << std::endl;
+
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int u = 0; u < 3; u++) {
+                underpromotion_actions[std::make_tuple(pos, dx, u)] = action;
+                actions[action++] = {UNDERPROMOTION_ACTION, pos, dx, u};
+            }
         }
-        return std::distance(arr, it);
-    };
-    // KNIGHT
-    if (piece == chess::piece::piece_knight){
-        std::pair<int, int> dir = std::make_pair(delta_x, delta_y);
-        size_t dir_idx = get_dir_idx(knight_directions, dir);
-        return dir_idx<<(6+3) | 0<<(6) | action;
     }
-    // ALL ELSE
-    int dir_x = delta_x != 0 ? delta_x / std::abs(delta_x) : 0;
-    int dir_y = delta_y != 0 ? delta_y / std::abs(delta_y) : 0;
-    std::pair<int, int> dir = std::make_pair(dir_x, dir_y);
-    size_t dir_idx = get_dir_idx(queen_directions, dir);
-    size_t magnitude = std::abs(delta_x);
-
-    return magnitude << (6+3) | dir_idx << (6) | action;
-
+    encoding_initialized = true;
 }
 
 
-
-chess::move Network::move_from_action(const chess::position& state, size_t action) {
-    chess::square from =  static_cast<chess::square>(action & ((1 << 6) - 1) );
-    int x = from % 8;
-    int y = from / 8;
-    bool underpromotion = 1 << (6+3+3) & action;
-    if (underpromotion) {
-        int dy = y == 1 ? -1 : 1;
-        size_t underprom_idx = ((1<<4) - 1) << 6;
-        underprom_idx >>= 6;
-        int dir = underprom_idx / 3;
-        int dx = dir - 1;
-        int piece_idx = underprom_idx % 3;
-        chess::piece piece;
-        switch(piece_idx){
-            case 0: piece = chess::piece::piece_knight; break;
-            case 1: piece = chess::piece::piece_bishop; break;
-            default: piece = chess::piece::piece_rook; 
+size_t Network::action_from_move(const chess::move& move) {
+    if (!encoding_initialized) {
+        initialize_encoding_map();
+    }
+    int delta_x = (move.to % 8) - (move.from % 8);
+    int delta_y = (move.to / 8) - (move.from / 8);
+    size_t pos = (size_t) move.from;
+    // UNDER PROMOTE
+    if (move.promote != chess::piece_none && move.promote != chess::piece_queen) {
+        size_t piece_idx = 0;
+        switch(move.promote){
+            case chess::piece_knight: piece_idx = 0; break;
+            case chess::piece_bishop: piece_idx = 1; break;
+            case chess::piece_rook: piece_idx = 2; break;
+            default: std::cerr << "Underpromotion is not valid" << std::endl;
         }
-        chess::square to = static_cast<chess::square>(x+dx + (y+dy)*8);
-        chess::move move{from, to};
-        move.promote = piece;
+        return underpromotion_actions[std::make_tuple(pos, delta_x, piece_idx)];
+    }
+    // KNIGHT
+    if (std::abs(delta_x) != std::abs(delta_y) && delta_x != 0 && delta_y != 0){
+        return knight_actions[std::make_tuple(pos, delta_x, delta_y)];
+    }
+    int dir_x = delta_x != 0 ? delta_x / std::abs(delta_x) : 0;
+    int dir_y = delta_y != 0 ? delta_y / std::abs(delta_y) : 0;
+    size_t magnitude = std::max(std::abs(delta_x), std::abs(delta_y));
+    return queen_actions[std::make_tuple(pos, dir_x, dir_y, magnitude)];
+}
+
+chess::move Network::move_from_action(const chess::position& state, size_t action_idx) {
+    if (!encoding_initialized) {
+        initialize_encoding_map();
+    }
+    Action action = actions[action_idx];
+    chess::square from = static_cast<chess::square>(action.pos);
+    size_t x = action.pos % 8;
+    size_t y = action.pos / 8;
+    if (action.type == UNDERPROMOTION_ACTION) {
+        int dx = action.v1;
+        size_t u = action.v2;
+        size_t y_promotion = y == 1 ? 0 : 7;
+        chess::square to = static_cast<chess::square>(x + dx + (y_promotion)*8);
+        chess::piece promote = underpromotions[u];
+        chess::move move{from, to, promote};
         return move;
     }
-    size_t magnitude = ((1<<3) - 1) << (6) & action;
-    magnitude >>= 6;
-    size_t dir = ((1<<3) - 1) << (6+3) & action;
-    dir >>= 6+3;
-    // KNIGHT
-    if (magnitude == 0) {
-        auto [dx, dy] = knight_directions[dir];
+    int dx = action.v1;
+    int dy = action.v2;
+    if (action.type == KNIGHT_ACTION) {
         chess::square to = static_cast<chess::square>(x + dx + (y + dy)*8);
-        return chess::move{from, to};
+        chess::move move{from, to};
+        return move;
     }
-
-    std::pair<int, int> xy_dir = queen_directions[dir];
-    int to_x = x + xy_dir.first * magnitude;
-    int to_y = y + xy_dir.second * magnitude;
-    chess::square to = static_cast<chess::square>(to_x + to_y*8);
+    size_t magnitude = action.v3;
+    dx *= magnitude;
+    dy *= magnitude;
+    int to_y = y + dy;
+    chess::square to = static_cast<chess::square>(x + dx + to_y*8);
     chess::move move{from, to};
-
-    auto [side, piece] = state.pieces().get(from); 
-    if (piece == chess::piece::piece_pawn && (to_y == 0 || to_y == 7)) {
+    auto [side, piece] = state.pieces().get(move.from);
+    if (piece == chess::piece_pawn && (to_y == 0 || to_y == 7)) {
         move.promote = chess::piece::piece_queen;
     }
     return move;
